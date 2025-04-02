a6331faeff89064b386870a2d662abbbb73f07ea
ClaireJung-A <cjungw730@gmail.com>
2025-04-01 22:29:18 -0400

    update work experience

--- data/about/experienceData.ts
+++ data/about/experienceData.ts
@@ -7,9 +7,9 @@ export const experienceData: experience[] = [
     startDate: "2024-05-01",
     endDate: "2024-08-01",
     points: [
-      "Led semantic analysis and query classification for a Retrieval-Augmented Generation (RAG) system, enhancing chatbot accuracy by 11%.",
-      "Engineered over 10 AI-driven prompts to optimize document retrieval, reducing hallucination rates by 17%.",
-      "Validated data pipelines with 30+ pytest test cases, improving API integrity and ensuring data reliability."
+      "Implemented keyword extraction for user queries using prompt engineering and custom data pipeline logic; captured key fields like product name and version to improve retrieval accuracy.",
+      "Investigated hallucinations in RAG-based chatbot caused by outdated certification data and  incorrect parsing of structured inputs; built a new pipeline using SQL, Python, and vector search to better handle data from PostgreSQL, collaborating with PMs and engineers  to ensure documentation was current.",
+      "Researched hallucination mitigation via multi-query retrieval strategies and graph databases on PostgreSQL vector stores; Built FastAPI endpoints to validate system behavior, and pushed chatbot responses to DBeaver for expert grading."
     ],
     urls: [""],
   },
@@ -19,9 +19,9 @@ export const experienceData: experience[] = [
     startDate: "2023-06-01",
     endDate: "2023-08-01",
     points: [
-      "Developed a web application with React, Redux, Express.js, and Node.js, secured with Docker and deployed on GCP.",
-      "Built 15+ reusable front-end components, improving UI consistency and reducing development time by 30%.",
-      "Analyzed customer payment trends using Pandas and PCA, driving a 3.2% increase in market reach and 4% revenue growth."
+      "Developed and deployed a full-stack web application using React, Redux, Express.js, and Node.js, featuring secure authentication, REST APIs, and Docker containerization, and deployment on AWS EC2.",
+      "Built 15+ reusable front-end components using React,, HTML, CSS, and Canva, enhancing UI/UX consistency and reducing development time for future features by 30%.",
+      "Unveiled customer payment preferences with Pandas and Principal component analysis(PCA), identifying trends that increased market reach by 3.2% and revenue by 4%, and visualized findings with Tableau."
     ],
     urls: [""],
   },
